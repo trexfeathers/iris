98036a48c66b94d82e356231129852fa25a78f22
Martin Yeo <40734014+trexfeathers@users.noreply.github.com>
2020-10-18 21:34:48 +0100

    Corrected ASV test data override location

--- lib/iris/tests/benchmarking/__init__.py
+++ lib/iris/tests/benchmarking/__init__.py
@@ -8,6 +8,6 @@ from os import environ
 from pathlib import Path
 
 ASV_ENV_DIR = Path(environ["ASV_ENV_DIR"])
-OVERRIDE_TEST_DATA_REPOSITORY = ASV_ENV_DIR / "iris-test-data" / "test_data"
+OVERRIDE_TEST_DATA_REPOSITORY = ASV_ENV_DIR / "lib" / "python3.7" / "site-packages" / "iris-test-data" / "test_data"
 environ["OVERRIDE_TEST_DATA_REPOSITORY"] = str(OVERRIDE_TEST_DATA_REPOSITORY)
 print(environ["OVERRIDE_TEST_DATA_REPOSITORY"])
