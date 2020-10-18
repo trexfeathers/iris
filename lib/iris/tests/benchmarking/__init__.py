8ee586ca3ad20f247d252b303c9e2ab19e31d7a8
Martin Yeo <40734014+trexfeathers@users.noreply.github.com>
2020-10-18 21:16:22 +0100

    ASV test data override.

--- lib/iris/tests/benchmarking/__init__.py
+++ lib/iris/tests/benchmarking/__init__.py
@@ -3,3 +3,9 @@
 # This file is part of Iris and is released under the LGPL license.
 # See COPYING and COPYING.LESSER in the root of the repository for full
 # licensing details.
+
+from os import environ
+from pathlib import Path
+
+ASV_ENV_DIR = environ["ASV_ENV_DIR"]
+environ["OVERRIDE_TEST_DATA_REPOSITORY"] = Path(ASV_ENV_DIR) / "iris-test-data" / "test_data"
